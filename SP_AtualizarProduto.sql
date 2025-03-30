36219863158a314aa68b840490490121cf61cb54
Bernardo Reis <101370121+BernardoReis28@users.noreply.github.com>
2025-03-30 11:51:12 +0100

    Update SP_AtualizarProduto.sql

--- SP_AtualizarProduto.sql
+++ SP_AtualizarProduto.sql
@@ -1,34 +1,46 @@
--- ================================================
--- Template generated from Template Explorer using:
--- Create Procedure (New Menu).SQL
---
--- Use the Specify Values for Template Parameters 
--- command (Ctrl-Shift-M) to fill in the parameter 
--- values below.
---
--- This block of comments will not be included in
--- the definition of the procedure.
--- ================================================
+USE [Producao]
+GO
+/****** Object:  StoredProcedure [dbo].[SP_InserirPecas]    Script Date: 30/03/2025 11:48:41 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
--- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
--- =============================================
-CREATE PROCEDURE <Procedure_Name, sysname, ProcedureName> 
-	-- Add the parameters for the stored procedure here
-	<@Param1, sysname, @p1> <Datatype_For_Param1, , int> = <Default_Value_For_Param1, , 0>, 
-	<@Param2, sysname, @p2> <Datatype_For_Param2, , int> = <Default_Value_For_Param2, , 0>
+--Declaração da SP
+ALTER PROCEDURE [dbo].[SP_InserirPecas]
+    @codigoPeca CHAR(8),
+    @dataProducao DATE,
+    @horaProducao TIME,
+    @tempoProducao INT,
+    @codigoResultado CHAR(2),
+    @dataTeste DATE           
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
+	--Verificar se já existe uma peça com o código atual e interrompe a inserção (caso haja)
+    IF EXISTS (SELECT 1 FROM Produto WHERE Codigo_Peca = @codigoPeca)
+    BEGIN
+        RAISERROR ('Produto com este código já existe já existe.', 16, 1);
+        RETURN;
+    END
 
-    -- Insert statements for procedure here
-	SELECT <@Param1, sysname, @p1>, <@Param2, sysname, @p2>
+	--Caso contrário, inserir peça na tabela "Produto" com os respetivos valores
+    INSERT INTO Produto (Codigo_Peca, Data_Producao, Hora_Producao, Tempo_Producao)
+    VALUES (@CodigoPeca, @DataProducao, @HoraProducao, @TempoProducao);
+    
+    --Recupera o ID do produto recém-inserido
+    DECLARE @NovoID INT;
+    SET @NovoID = SCOPE_IDENTITY(); --Retorna o ID do último ID inserido na tabela
+    
+    --Se não for informado o código do teste, define como "06" (Desconhecido)
+    IF (@codigoResultado IS NULL)
+        SET @codigoResultado = '06';
+    
+    --Se não for informada a data do teste, usar a data atual
+    IF (@dataTeste IS NULL)
+        SET @dataTeste = CONVERT(DATE, GETDATE());
+    
+    --Inserir o registo do teste na tabela Testes
+    INSERT INTO Testes (ID_Produto, Codigo_Resultado, Data_Teste)
+    VALUES (@NovoID, @codigoResultado, @dataTeste);
+    
+    SELECT @NovoID AS ProdutoID, 'Inserção realizada com sucesso' AS Mensagem;
 END
-GO
